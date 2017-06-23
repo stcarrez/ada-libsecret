da73be947e2baa57f95454b0b20b7847ba39a506
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-06-23 21:05:50 +0200

    Define root package for the library

---
+++ src/secret.ads
@@ -0,0 +1,56 @@
+-----------------------------------------------------------------------
+--  Secret -- Ada wrapper for Secret Service
+--  Copyright (C) 2017 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with System;
+with Ada.Finalization;
+private with Interfaces.C.Strings;
+package Secret is
+
+   type Object_Type is tagged private;
+
+private
+
+   use type System.Address;
+
+   subtype Opaque_Type is System.Address;
+
+   type Object_Type is new Ada.Finalization.Controlled with record
+      Opaque : Opaque_Type := System.Null_Address;
+   end record;
+
+   procedure Set_Opaque (Into : in out Object_Type'Class;
+                         Data : in Opaque_Type);
+
+   function Get_Opaque (From : in Object_Type'Class) return Opaque_Type;
+
+   subtype Chars_Ptr is Interfaces.C.Strings.chars_ptr;
+
+   procedure Free (P : in out Chars_Ptr)
+      renames Interfaces.C.Strings.Free;
+
+   function To_String (P : Chars_Ptr) return String
+      renames Interfaces.C.Strings.Value;
+
+   function New_String (V : in String) return Chars_Ptr
+      renames Interfaces.C.Strings.New_String;
+
+   pragma Linker_Options ("-lsecret-1");
+   pragma Linker_Options ("-lglib-2.0");
+   pragma Linker_Options ("-lgio-2.0");
+   pragma Linker_Options ("-gobject");
+   pragma Linker_Options ("-liconv");
+end Secret;
